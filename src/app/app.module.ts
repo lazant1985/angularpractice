847e2cd1d3f7002e1b5b5fa3b599c7983873e499
lazant85 <alazareuski@collab-it.com>
2020-07-03 11:36:34 +0300

    my new store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{path: '', component: ProductListComponent}]),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
